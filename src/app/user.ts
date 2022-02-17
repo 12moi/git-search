49107e6c721938e8926b342798cb185c59e50865
12moi <moi.shadrack@student.moringaschool.com>
2022-02-17 19:07:47 +0300

    initial commit

---
+++ src/app/user.ts
@@ -0,0 +1,6 @@
+export class User {
+    static results:any;
+    constructor( public name:string, public url:string, public login:string, public location:string, public public_repos:number, public followers:number, public  following:number, public created_at:Date){
+
+    }
+}
