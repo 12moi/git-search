93c23757df2bf208dfeb4341129a90f6e3699d9c
12moi <moi.shadrack@student.moringaschool.com>
2022-02-18 14:57:55 +0300

    add promise logic to user service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  accessToken:'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'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  accessToken:'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'
 };
 
 /*
